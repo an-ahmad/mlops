4b1e44967fde0fad30daa00a069937961ff2ce23
an-ahmad <ahmednisar93@gmail.com>
2021-09-06 21:00:00 +0200

    src/get_data and dvc yaml  added

---
+++ src/get_data.py
@@ -0,0 +1,30 @@
+import os
+import yaml
+import pandas as pd
+import argparse
+import pickle
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def get_data(config_path):
+    config = read_params(config_path)
+    # print(config)
+    data_path = config["data_source"]["sem_source"]
+    df = pickle.load(open(data_path, 'rb'))
+
+    return df
+
+
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    data = get_data(config_path=parsed_args.config)
+
+
+
+
