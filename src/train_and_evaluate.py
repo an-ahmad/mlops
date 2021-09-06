8e2ee668ab45f90d4182bb093edb4d7b188199a0
an-ahmad <ahmednisar93@gmail.com>
2021-09-07 00:58:38 +0200

    tracker added

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -73,10 +73,10 @@ def train_and_evaluate(config_path):
         json.dump(params, f, indent=4)
     #####################################################
 
-    # os.makedirs(model_dir, exist_ok=True)
-    # model_path = os.path.join(model_dir, "model.joblib")
-    #
-    # joblib.dump(model, model_path)
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.joblib")
+
+    joblib.dump(model, model_path)
 
 
 if __name__ == "__main__":
